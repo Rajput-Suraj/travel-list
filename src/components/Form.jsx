9026de3f1295f11367cc2aba901b366fed5498b9
Rajput-Suraj <surajarajput@gmail.com>
2023-10-14 12:14:01 +0530

    Build form elementd and handled form submission

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
 const Form = () => {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😊 trip?</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 };
 
