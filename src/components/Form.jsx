d713d74121fb1e56c37a3b226dfd35c5619dd20d
Rajput-Suraj <surajarajput@gmail.com>
2023-10-14 12:44:56 +0530

    Updated form elements to be controlled elements

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+  const [quantity, setQuantity] = useState(1);
+  const [description, setDescription] = useState('');
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    //If we don't have description return immediately
+    if (!description) return;
+
+    // Create a new Item object
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
+    };
+
+    console.log({ newItem });
+
+    //Clear the form data
+    setQuantity(1);
+    setDescription('');
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😊 trip?</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
@@ -16,7 +36,12 @@ const Form = () => {
         ))}
       </select>
 
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
