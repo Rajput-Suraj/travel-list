c26e08e5fb8809016da3807a31b3115f6dbb380f
Rajput-Suraj <surajarajput@gmail.com>
2023-10-21 15:05:08 +0530

    Added sort by functionality and updated footer

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const Stats = ({ items }) => {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
   const percentage = Math.round((numPacked / numItems) * 100);
