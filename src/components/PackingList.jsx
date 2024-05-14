c7b277016161ae6d68f5a0b62fa0f5e2583c2002
Suraj Rajput <49227244+Rajput-Suraj@users.noreply.github.com>
2024-05-14 13:27:38 +0530

    Update PackingList.jsx

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -6,6 +6,7 @@ import Item from './Item';
 //   { id: 1, description: 'Passports', quantity: 2, packed: false },
 //   { id: 2, description: 'Socks', quantity: 12, packed: true },
 //   { id: 3, description: 'Charger', quantity: 1, packed: false },
+
 // ];
 
 const PackingList = ({ items, onDeleteItem, onToggleItems, onClearList }) => {
